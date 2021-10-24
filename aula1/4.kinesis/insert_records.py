9bb29b2f715d085f5ed926cc32e623d00fe343ba
Vitor Loures <vitorf.loures@gmail.com>
2021-10-23 21:03:02 -0300

    Revisão da aula 1

--- aula1/4.kinesis/insert_records.py
+++ aula1/4.kinesis/insert_records.py
@@ -6,10 +6,14 @@ from fake_web_events import Simulation
 client = boto3.client('firehose')
 
 
+# OBS: Firehose é um serviço de ingestão de dados batch ou streaming,
+# semelhante a uma versão simples do Kafka, gerenciado pela AWS.
+# Ele permite de forma simples inserir dados em datalake, datawarehouse,
+# ou plataforma de dados análogas.
 def put_record(event):
     data = json.dumps(event) + "\n"
     response = client.put_record(
-        DeliveryStreamName='kinesis-firehose-belisco',
+        DeliveryStreamName='kinesis-firehose-vitor-loures',
         Record={"Data": data}
     )
     print(event)
